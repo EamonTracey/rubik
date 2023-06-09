5ecdcd0726634dc98893c0b8519820509784389b
eamontracey <etracey@nd.edu>
2023-06-08 23:31:39 -0400

    restructure turn files

---
+++ rubik/rubik/Logic/Turn/Turn.swift
@@ -0,0 +1,12 @@
+enum Turn {
+    /*
+     * The cube has 6 outer layers which can be turned.
+     */
+    
+    case up(TurnDegree)
+    case down(TurnDegree)
+    case right(TurnDegree)
+    case left(TurnDegree)
+    case front(TurnDegree)
+    case back(TurnDegree)
+}
