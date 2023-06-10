a6e73ad21c0af46ee8067af1c3f7631d285f243c
eamontracey <etracey@nd.edu>
2023-06-10 00:53:35 -0400

    add algorithm repeats option

--- rubik/rubik/Logic/Cube/Cube+Algorithm.swift
+++ rubik/rubik/Logic/Cube/Cube+Algorithm.swift
@@ -3,9 +3,11 @@ extension Cube {
      * Apply an algorithm to a cube.
      */
     
-    mutating func execute(algorithm: Algorithm) {
-        for turn in algorithm.turns {
-            self.turn(turn)
+    mutating func execute(_ algorithm: Algorithm, repeats: UInt = 1) {
+        for _ in 0..<repeats {
+            for turn in algorithm.turns {
+                self.turn(turn)
+            }
         }
     }
 }
