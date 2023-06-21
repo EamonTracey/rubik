01d962a93ea8592f2082c36ecb6a42286566d0f3
eamontracey <etracey@nd.edu>
2023-06-20 22:08:26 -0400

    add documentation for cube

--- Sources/Rubik/Cube.swift
+++ Sources/Rubik/Cube.swift
@@ -1,17 +1,45 @@
+/// A representation of the 3x3 Rubik's cube twisty puzzle.
+///
+/// The state of a cube is defined by the orientation and permutation of its 12 edges
+/// and the orientation and permutation of its 8 corners. In the ``edges`` and ``corners``
+/// arrays, the index of each cubelet corresponds to its position in the cube. Therefore, the order
+/// of the arrays defines the cube's permutation. A cube with the correct permutation will have the
+/// ``edges`` and ``corners`` arrays sorted ascendingly by each cubelet's `solvedPosition`
+/// (each index matches the corresponding `solvedPosition.rawValue`).
+///
+/// A convenient way to initialize a cube with a specific state is to scramble the ``solvedCube``
+/// with an ``Algorithm``. For example:
+///
+///     var cube: Cube = .solvedCube
+///     cube.execute(Algorithm("R U R' U' R' F R2 U' R' U' R U R' F'")!)
+///
+/// - Note: A cube is valid if and only if the ``edges`` and ``corners`` arrays contain 12 and 8
+/// cubelets, respectively, with unique `solvedPosition` values. Otherwise, the cube is invalid, and
+/// operations on the cube are undefined. Observe that a cube can be valid yet unsolvable.
 public struct Cube {
+    /// The edges of a 3x3 Rubik's cube.
+    ///
+    /// - Note: This array must contain 12 instances of ``Edge`` with unique `solvedPosition` values to be valid.
     public var edges: [Edge]
+    
+    /// The corners of a 3x3 Rubik's cube.
+    ///
+    /// - Note: This array must contain 8 instances of ``Corner`` with unique `solvedPosition` values to be valid.
     public var corners: [Corner]
 }
 
 public extension Cube {
+    /// The ``edges`` array that corresponds to a solved cube.
     static let solvedEdges: [Edge] = Cube.Edge.Position.allCases.map { position in
         Edge(orientation: .correct, solvedPosition: position)
     }
     
+    /// The ``corners`` array that corresponds to a solved cube.
     static let solvedCorners: [Corner] = Cube.Corner.Position.allCases.map { position in
         Corner(orientation: .correct, solvedPosition: position)
     }
     
+    /// The cube in the solved state.
     static let solvedCube: Cube = Cube(edges: Cube.solvedEdges, corners: Cube.solvedCorners)
 }
 
