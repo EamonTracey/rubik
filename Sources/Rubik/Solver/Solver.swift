d9114d9f373b7766f42e2371e859902ac39d34ce
eamontracey <etracey@nd.edu>
2023-06-27 22:34:52 -0400

    readd solve method enum

--- Sources/Rubik/Solver/Solver.swift
+++ Sources/Rubik/Solver/Solver.swift
@@ -1,9 +1,15 @@
 public class Solver {
     @inlinable
-    public func solve(_ cube: Cube, using method: SolveMethod) -> Algorithm {
+    public func solve(_ cube: Cube, using method: Solver.Method) -> Algorithm {
         switch method {
         case .thistlethwaite:
             return self.solveThistlethwaite(cube)
         }
+    }
+}
+
+extension Solver {
+    public enum Method {
+        case thistlethwaite
     }
 }
