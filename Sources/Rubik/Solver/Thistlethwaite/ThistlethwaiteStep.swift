2f798ac41be19952a86e6f6c63ec375503875628
eamontracey <etracey@nd.edu>
2023-06-29 11:49:47 -0400

    readd traversal to step

--- Sources/Rubik/Solver/Thistlethwaite/ThistlethwaiteStep.swift
+++ Sources/Rubik/Solver/Thistlethwaite/ThistlethwaiteStep.swift
@@ -1,3 +1,4 @@
+import Collections
 import Foundation
 
 protocol ThistlethwaiteStep {
@@ -9,11 +10,30 @@ protocol ThistlethwaiteStep {
 
 extension ThistlethwaiteStep {
     static func generateTable() -> [Int: String] {
-        return Solver.Thistlethwaite.traversal(
-            factor: factor,
-            allowedTurns: allowedTurns,
-            stateEncoding: encode
-        )
+        var statesTable: [Int: String] = Dictionary(minimumCapacity: factor)
+        var frontier: Deque<(cube: Cube, algorithm: Algorithm)> = [(.solvedCube, .nothing)]
+
+        while statesTable.count < factor, let node = frontier.popFirst() {
+            let state = encode(node.cube)
+
+            if statesTable.keys.contains(state) {
+                continue
+            }
+
+            statesTable[state] = node.algorithm.reversed.stringNotation
+
+            for turn in allowedTurns {
+                if let lastTurn = node.algorithm.turns.last, turn.sameLayer(as: lastTurn) {
+                    continue
+                }
+                var adjacentNode = node
+                adjacentNode.cube.turn(turn)
+                adjacentNode.algorithm.append(turn)
+                frontier.append(adjacentNode)
+            }
+        }
+
+        return statesTable
     }
 }
 
