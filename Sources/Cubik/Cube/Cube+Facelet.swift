a30683030fcd4be82ea407e7743882df29764749
eamontracey <etracey@nd.edu>
2023-07-15 00:11:05 -0400

    implement edge facelets

--- Sources/Cubik/Cube/Cube+Facelet.swift
+++ Sources/Cubik/Cube/Cube+Facelet.swift
@@ -2,8 +2,26 @@ extension Cube {
     public init?(facelets: [Facelet]) {
         guard facelets.count == 54 else { return nil }
 
-        // TODO: Implement
-        self = .solvedCube
+        self.corners = Cube.solvedCorners
+
+        guard let upRight = Edge(facelets[5], facelets[19]) else { return nil }
+        guard let upLeft = Edge(facelets[3], facelets[28]) else { return nil }
+        guard let upFront = Edge(facelets[7], facelets[37]) else { return nil }
+        guard let upBack = Edge(facelets[1], facelets[46]) else { return nil }
+        guard let downRight = Edge(facelets[14], facelets[25]) else { return nil }
+        guard let downLeft = Edge(facelets[12], facelets[34]) else { return nil }
+        guard let downFront = Edge(facelets[10], facelets[43]) else { return nil }
+        guard let downBack = Edge(facelets[16], facelets[52]) else { return nil }
+        guard let rightFront = Edge(facelets[41], facelets[21]) else { return nil }
+        guard let rightBack = Edge(facelets[48], facelets[23]) else { return nil }
+        guard let leftFront = Edge(facelets[39], facelets[32]) else { return nil }
+        guard let leftBack = Edge(facelets[50], facelets[30]) else { return nil }
+
+        self.edges = [
+            upRight, upLeft, downRight, downLeft,
+            upFront, upBack, downFront, downBack,
+            rightFront, rightBack, leftFront, leftBack
+        ]
     }
 }
 
@@ -69,29 +87,35 @@ extension Cube.Edge {
             self.orientation = .flipped
         case (.right, .front):
             self.solvedPosition = .rightFront
-            self.orientation = .correct
+            self.orientation = .flipped
         case (.front, .right):
             self.solvedPosition = .rightFront
-            self.orientation = .flipped
+            self.orientation = .correct
         case (.right, .back):
             self.solvedPosition = .rightBack
-            self.orientation = .correct
+            self.orientation = .flipped
         case (.back, .right):
             self.solvedPosition = .rightBack
-            self.orientation = .flipped
+            self.orientation = .correct
         case (.left, .front):
             self.solvedPosition = .leftFront
-            self.orientation = .correct
+            self.orientation = .flipped
         case (.front, .left):
             self.solvedPosition = .leftFront
-            self.orientation = .flipped
+            self.orientation = .correct
         case (.left, .back):
             self.solvedPosition = .leftBack
-            self.orientation = .correct
+            self.orientation = .flipped
         case (.back, .left):
             self.solvedPosition = .leftBack
-            self.orientation = .flipped
+            self.orientation = .correct
         default: return nil
         }
+    }
+}
+
+extension Cube.Corner {
+    init?(_ faceletA: Facelet, _ faceletB: Facelet) {
+        return nil
     }
 }
---
+++ Tests/CubikTests/CubikFaceletTests.swift
@@ -0,0 +1,97 @@
+import XCTest
+
+@testable import Cubik
+
+class CubikFaceletTests: XCTestCase {
+    func testFacelet() {
+        // Solved.
+        var cube = Cube(facelets: [
+            .up, .up, .up, .up, .up, .up, .up, .up, .up,
+            .down, .down, .down, .down, .down, .down, .down, .down, .down,
+            .right, .right, .right, .right, .right, .right, .right, .right, .right,
+            .left, .left, .left, .left, .left, .left, .left, .left, .left,
+            .front, .front, .front, .front, .front, .front, .front, .front, .front,
+            .back, .back, .back, .back, .back, .back, .back, .back, .back
+        ])!
+        XCTAssert(cube.isValid)
+        XCTAssertEqual(cube, .solvedCube)
+
+        // U
+        cube = Cube(facelets: [
+            .up, .up, .up, .up, .up, .up, .up, .up, .up,
+            .down, .down, .down, .down, .down, .down, .down, .down, .down,
+            .back, .back, .back, .right, .right, .right, .right, .right, .right,
+            .front, .front, .front, .left, .left, .left, .left, .left, .left,
+            .right, .right, .right, .front, .front, .front, .front, .front, .front,
+            .left, .left, .left, .back, .back, .back, .back, .back, .back
+        ])!
+        XCTAssert(cube.isValid)
+        cube.turn(.up(.counterclockwise))
+        XCTAssertEqual(cube.edges, Cube.solvedEdges)
+
+        // D
+        cube = Cube(facelets: [
+            .up, .up, .up, .up, .up, .up, .up, .up, .up,
+            .down, .down, .down, .down, .down, .down, .down, .down, .down,
+            .right, .right, .right, .right, .right, .right, .front, .front, .front,
+            .left, .left, .left, .left, .left, .left, .back, .back, .back,
+            .front, .front, .front, .front, .front, .front, .left, .left, .left,
+            .back, .back, .back, .back, .back, .back, .right, .right, .right
+        ])!
+        XCTAssert(cube.isValid)
+        cube.turn(.down(.counterclockwise))
+        XCTAssertEqual(cube.edges, Cube.solvedEdges)
+
+        // R
+        cube = Cube(facelets: [
+            .up, .up, .front, .up, .up, .front, .up, .up, .front,
+            .down, .down, .back, .down, .down, .back, .down, .down, .back,
+            .right, .right, .right, .right, .right, .right, .right, .right, .right,
+            .left, .left, .left, .left, .left, .left, .left, .left, .left,
+            .front, .front, .down, .front, .front, .down, .front, .front, .down,
+            .up, .back, .back, .up, .back, .back, .up, .back, .back
+        ])!
+        XCTAssert(cube.isValid)
+        cube.turn(.right(.counterclockwise))
+        XCTAssertEqual(cube.edges, Cube.solvedEdges)
+
+        // L
+        cube = Cube(facelets: [
+            .back, .up, .up, .back, .up, .up, .back, .up, .up,
+            .front, .down, .down, .front, .down, .down, .front, .down, .down,
+            .right, .right, .right, .right, .right, .right, .right, .right, .right,
+            .left, .left, .left, .left, .left, .left, .left, .left, .left,
+            .up, .front, .front, .up, .front, .front, .up, .front, .front,
+            .back, .back, .down, .back, .back, .down, .back, .back, .down
+        ])!
+        XCTAssert(cube.isValid)
+        cube.turn(.left(.counterclockwise))
+        XCTAssertEqual(cube.edges, Cube.solvedEdges)
+
+        // F
+        cube = Cube(facelets: [
+            .up, .up, .up, .up, .up, .up, .left, .left, .left,
+            .right, .right, .right, .down, .down, .down, .down, .down, .down,
+            .up, .right, .right, .up, .right, .right, .up, .right, .right,
+            .left, .left, .down, .left, .left, .down, .left, .left, .down,
+            .front, .front, .front, .front, .front, .front, .front, .front, .front,
+            .back, .back, .back, .back, .back, .back, .back, .back, .back
+        ])!
+        XCTAssert(cube.isValid)
+        cube.turn(.front(.counterclockwise))
+        XCTAssertEqual(cube.edges, Cube.solvedEdges)
+
+        // B
+        cube = Cube(facelets: [
+            .right, .right, .right, .up, .up, .up, .up, .up, .up,
+            .down, .down, .down, .down, .down, .down, .left, .left, .left,
+            .right, .right, .down, .right, .right, .down, .right, .right, .down,
+            .up, .left, .left, .up, .left, .left, .up, .left, .left,
+            .front, .front, .front, .front, .front, .front, .front, .front, .front,
+            .back, .back, .back, .back, .back, .back, .back, .back, .back
+        ])!
+        XCTAssert(cube.isValid)
+        cube.turn(.back(.counterclockwise))
+        XCTAssertEqual(cube.edges, Cube.solvedEdges)
+    }
+}
